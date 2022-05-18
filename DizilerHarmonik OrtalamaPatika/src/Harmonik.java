8ee89965b26ad36225e49c23b7f126226f23feee
Can Yeşiltepe <cyesiltepe9@gmail.com>
2022-05-18 12:31:30 +0300

    .
    .

---
+++ DizilerHarmonik OrtalamaPatika/src/Harmonik.java
@@ -0,0 +1,27 @@
+public class Harmonik {
+
+
+    public static void main(String[] args) {
+
+        double ort=0;
+        double [] asd = {1,2,3,4,5,6};
+
+        for (int i = 0; i<asd.length;i++){
+
+            ort+=(1/asd[i]);
+
+        }
+        System.out.println(ort);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
